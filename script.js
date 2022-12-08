e21e5611499589aec81ac60fbeba095df47bb81f
Shawn19702 <shawnboodhoo5@gmail.com>
2022-12-07 23:48:18 -0500

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('button');
+const btn = document.querySelector('.button10');
 btn.addEventListener('click', function(){
-    window.open("https://www.linkedin.com/in/mikailaakeredolu/", "_blank")
+    window.open("https://www.linkedin.com/in/shawn-boodhoo-98115324b/", "_blank")
 });
